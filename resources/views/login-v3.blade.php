b1f123679c1698de0365797e4e1754bd225907cf
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-04-27 12:04:48 +0700

    update code

--- resources/views/login-v3.blade.php
+++ resources/views/login-v3.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Login</title>
 
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/education_2.svg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
